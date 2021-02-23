9092489d39be8f522a02a574a0bb489cb34ca051
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-02-23 20:35:14 +0100

    Setting up Celery for asynchronous tasks

---
+++ d3be_myshop/celery.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTING_MODULE', 'd3be_myshop.settings')
+
+app = Celery('d3be_myshop')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
